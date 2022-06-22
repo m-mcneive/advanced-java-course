af3096f77f54cdb6f54e45c08c2463893b59bc45
Matt McNeive <matt.mcneive@gmail.com>
2022-06-22 17:07:09 -0500

    Fix error

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,6 @@ package main.java;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!");k
+        System.out.println("Hello World!");
     }
 }
